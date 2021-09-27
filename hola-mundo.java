186b325131639249ad547cc32fd844e86b6c0d94
JoseJluioLopezValero <jjuliolopezv@iesalixar.org>
2021-09-27 14:08:21 +0200

    Comentarios en documento

--- hola-mundo.java
+++ hola-mundo.java
@@ -1,3 +1,12 @@
+/**
+Muestra en la pantalla hola mundo
+
+@JoseJulioLopezValero
+**/
+
+
+
+
 public class Holamundo {
 	public static void main(string[] args)
 		System.out.println("Hola Mundo");	
